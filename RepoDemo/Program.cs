642213be98a1191f135166bfa9c63f0493b33113
Peter Lillevold <peter.lillevold@4subsea.com>
2020-02-26 18:17:24 +0100

    Initial commit

---
+++ RepoDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RepoDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Branch World!");
+            Console.ReadLine();
+        }
+    }
+}
